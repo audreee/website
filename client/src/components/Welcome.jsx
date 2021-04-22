50694f1a03ad678c41624776f091792f6ddba14e
Audree Steinberg <audreesteinberg@gmail.com>
2021-04-21 17:48:24 -0700

    Add slideIn transitions to hello message and punctuation

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -8,8 +8,10 @@ let Welcome = () => {
         <div className="welcome-right"></div>
       </div>
       <div className="welcome-overlay">
-        <div className="welcome-text fadeIn">
-          <h1>hello</h1>
+        <div className="welcome-text">
+          <div className="slideFromLeft">
+            <h1>hello</h1>
+          </div>
           <h1 className="period slideFromRight">.</h1>
         </div>
       </div>
