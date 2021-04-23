9b175b22a16a15273d501500e7fe4532d9dbaad2
Audree Steinberg <audreesteinberg@gmail.com>
2021-04-22 17:26:34 -0700

    Add projects to Projects section. Conditionally render button user hovers over card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,24 +4,24 @@ class Card extends React.Component {
   constructor() {
     super();
     this.state = {
-      isFlipped: false
+      hover: false
     }
     this.handleMouseMove = this.handleMouseMove.bind(this);
   }
 
   handleMouseMove() {
-    this.setState({ isFlipped: !this.state.isFlipped })
+    this.setState({ hover: !this.state.hover })
   }
 
   render() {
     return (
-     <div className="card">
+     <div className="card" onMouseEnter={this.handleMouseMove} onMouseLeave={this.handleMouseMove}>
       <div className="card-img" style={{backgroundImage: `url("${this.props.img}")`}}>
       </div>
       <div className="card-content">
         <h5 className="card-title">{this.props.title}</h5>
         <p className="card-description">{this.props.description}</p>
-        <button className="card-button"><a href={this.props.link}>See More</a></button>
+        {this.state.hover && <button className="card-button"><a href={this.props.link}>See More</a></button>}
       </div>
      </div>
     )
--- client/src/data/projectData.js
+++ client/src/data/projectData.js
@@ -1,11 +1,25 @@
-import shopLocal from './images/shop-local.png';
+import shopLocalImage from './images/shop-local.png';
+import reviewsImage from './images/small-reviews.png';
+import graphImage from './images/stress-graph.png';
 
 let projectData = [
   {
-    img: shopLocal,
+    img: reviewsImage,
+    title: 'Ratings & Reviews',
+    description: "The ratings & reviews section for the product page on a retail website. Built using React, this app includes a pop-up modal, dynamic graphs, hoverable stars, and a list of reviews with a smooth, 'infinite' scroll. It's responsive for both mobile and desktop viewing and achieved a 95 Lighthouse score.",
+    link: 'https://github.com/audreee/ratings-and-reviews'
+  },
+  {
+    img: shopLocalImage,
     title: 'Shop Local',
-    description: 'A local business finder that finds small businesses owned by members of vulnerable communities. This app was built in under 2 days as part of a challenge to build a minimum viable product in less than 2 days. The search engine was built using PostgreSQL and the maps is powered by the Google Maps API',
+    description: 'A local business finder that finds small businesses owned by members of vulnerable communities. This app was built in under 2 days as part of a challenge to build a minimum viable product in less than 2 days. The search engine was built using PostgreSQL and the maps is powered by the Google Maps API.',
     link: 'https://github.com/audreee/shop-local'
+  },
+  {
+    img: graphImage,
+    title: 'System Design',
+    description: "A back end API for the Questions & Answers data of an e-commerce website. This app was built to scale using Express/Node, PostgreSQL, Docker, and deployed to AWS EC2 micro instances. The SQL queries return data in nested JSON objects. It supports 0 - 10,000rps in 20 seconds with a 0% error rate.",
+    link: 'https://github.com/audreee/system-design'
   }
 ]
 
