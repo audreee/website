6b898b53219639b283971d368f130e1e0cd88dda
Audree Steinberg <audreesteinberg@gmail.com>
2021-04-22 23:29:13 -0700

    Change contact page styling to a card

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -7,15 +7,21 @@ import headshot from '../data/images/headshot.jpeg';
 let Contact = () => {
   return(
     <div className="contact-wrapper">
-      <h3>CONTACT</h3>
-      <img src={headshot} alt="Headshot of Audree" />
-      <h3>AUDREE STEINBERG</h3>
-      <h5>Full Stack Engineer</h5>
-      <div className="row icons">
-        <a href={'http://github.com/audreee'}><FontAwesomeIcon icon={faGithub} size="4x" style={{color: '#FAD02C'}}/></a>
-        <a href={'http://linkedin.com/in/audreesteinberg'}><FontAwesomeIcon icon={faLinkedinIn} size="4x" style={{color: '#282120'}}/></a>
-        <a href={'mailto:audreecodes@gmail.com'}><FontAwesomeIcon icon={faEnvelope} size="4x" style={{color: '#F8EFE4'}}/></a>
+      <div className="column">
+      <h3 className="contact-title">CONTACT</h3>
+        <img src={headshot} alt="Headshot of Audree" />
+        <div className="contact-text">
+          <h3>AUDREE STEINBERG</h3>
+          <h5>Full Stack Engineer</h5>
+        </div>
+
+        <div className="row icons">
+          <a href={'http://github.com/audreee'}><FontAwesomeIcon icon={faGithub} size="4x" style={{color: '#FAD02C'}}/></a>
+          <a href={'http://linkedin.com/in/audreesteinberg'}><FontAwesomeIcon icon={faLinkedinIn} size="4x" style={{color: '#282120'}}/></a>
+          <a href={'mailto:audreecodes@gmail.com'}><FontAwesomeIcon icon={faEnvelope} size="4x" style={{color: '#90ADC6'}}/></a>
+        </div>
       </div>
+
     </div>
   )
 }
